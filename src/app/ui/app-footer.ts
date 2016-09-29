48e6dd8386757601662ca07aed2a298482b4dafd
peel3r <ismc2222@gmail.com>
2016-09-29 21:17:13 +0100

    adds footer

---
+++ src/app/ui/app-footer.ts
@@ -0,0 +1,74 @@
+import {Component} from '@angular/core';
+import {AuthService} from '../services'
+import {ToastsManager} from "ng2-toastr";
+import {DiaryService} from "../services/painDiaries";
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-footer',
+  styles: [`
+  .app-bar {
+  height: 140px;
+  padding: 5px 30px;
+  background-color: #00BCD4;
+}
+.logo {
+  color: white;
+  font-size: 30px;
+  font-weight: 300;
+  cursor: pointer;
+}
+.link {
+  color: white;
+  font-size: 24px;
+  font-weight: 400; 
+  cursor: pointer; 
+}
+`],
+  templateUrl: './app-footer.template.html'
+})
+
+export class AppFooter {
+  mode: string;
+  library: string = ''
+  user_id = window.localStorage.getItem('cpms_user_id')
+  user_name = window.localStorage.getItem('cpms_user_name')
+
+
+
+  constructor(
+                private authService: AuthService,
+                private router: Router,
+                private diaryService: DiaryService,
+                public toastr: ToastsManager){
+
+  }
+
+  showSuccess() {
+    this.toastr.success('Successfully logged out');
+  }
+
+  ngDoCheck() {
+
+    if (localStorage.getItem('retain_token')) {
+      this.mode = 'signout'
+      this.library = 'Library'
+    } else {
+      this.mode = ''
+      this.library = ''
+    }
+  }
+
+  changeMode() {
+    if (this.mode == 'signin') {
+      this.mode = 'signout'
+    } else {
+      this.mode = 'signin'
+      this.authService.signout()
+    }
+  }
+
+
+  signout() {
+    this.authService.signout()
+  }
+}
