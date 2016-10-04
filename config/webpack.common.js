47ca0617df6604feb4cfaf618b419aec8d759eb8
peel3r <ismc2222@gmail.com>
2016-10-04 11:13:28 +0100

    updated webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -94,9 +94,10 @@ module.exports = function(options) {
        *
        * See: http://webpack.github.io/docs/configuration.html#module-preloaders-module-postloaders
        */
-      preLoaders: [
+      rules: [
         {
           test: /\.ts$/,
+          enforce: "pre",
           loader: 'string-replace-loader',
           query: {
             search: '(System|SystemJS)(.*[\\n\\r]\\s*\\.|\\.)import\\((.+)\\)',
@@ -198,7 +199,11 @@ module.exports = function(options) {
         filename: 'webpack-assets.json',
         prettyPrint: true
       }),
-
+      new webpack.LoaderOptionsPlugin({
+           options: {
+          context: __dirname
+        }
+    }),
       /*
        * Plugin: ForkCheckerPlugin
        * Description: Do type checking in a separate process, so webpack don't need to wait.
