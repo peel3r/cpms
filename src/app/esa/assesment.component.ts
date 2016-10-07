8132957db3117676b18b0ed26046ea5687e53a4e
peel3r <ismc2222@gmail.com>
2016-10-07 20:12:10 +0100

    refactores question creator

--- src/app/esa/assesment.component.ts
+++ src/app/esa/assesment.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Question } from './question.component';
-import {QuestionCreator} from './question-creator'
 @Component({
   selector: 'question-container',
 
@@ -15,22 +14,22 @@ import {QuestionCreator} from './question-creator'
     }
   `],
   template: `
-    <div class="row center-xs questions">
+    
       <div>
-    <question-creator (createQuestion)="onCreateQuestion($event)"></question-creator>
+        <question-creator (createQuestion)="onCreateQuestion($event)"></question-creator>
       </div>
       <div class="questions col-xs-8">
         <div class="row between-xs">
-          <!--<question-card-->
-            <!--class="col-xs-4"-->
-            <!--[question]="question"-->
-            <!--*ngFor="let question of questions; let i = index"-->
-            <!--(checked)="onQuestionChecked($event, i)"-->
-          <!--&gt;-->
-          <!--</question-card>-->
+          <question-card
+            class="col-xs-4"
+            [question]="question"
+            *ngFor="let question of questions; let i = index"
+            (checked)="onQuestionChecked($event, i)"
+          >
+          </question-card>
         </div>
       </div>
-    </div>
+
   `
 })
 export class Esa {
--- src/app/esa/question-creator.ts
+++ src/app/esa/question-creator.ts
@@ -24,41 +24,61 @@ Input,
   templateUrl: './question.template.html'
 })
 export class QuestionCreator {
-  @Output() createQuestion = new EventEmitter();
+  @Output() createAssessment = new EventEmitter();
   newQuestion = {
     title: '',
     value: ''
   };
+  newAssessment ={
+    question_1: ''
+  }
+  questions = [
+    {text: 'Mobilising unaided by another person with or without a walking stick, manual wheelchair or other aid if such aid is normally, or could reasonably be, worn or used. ', answers: [
+      {id: 1, answerText: 'Cannot either (i) mobilise more than 50 meters on level ground without stopping in order to avoid significant discomfort or exhaustion or (ii) repeatedly mobilise 50 meters within a reasonable timescale because of significant discomfort or exhaustion.', answerPoints: 15},
+      {id: 2, answerText: 'Cannot mount or descend two steps unaided by another person even with the support of a handrail. ', answerPoints: 9},
+      {id: 3, answerText: 'Cannot either (i) mobilise more than 100 meters on level ground without stopping in order to avoid significant discomfort or exhaustion or (ii) repeatedly mobilise 100 meters within a reasonable timescale because of significant discomfort or exhaustion.', answerPoints: 9},
+      {id: 4, answerText: 'Cannot either (i) mobilise more than 200 metres on level ground without stopping in order to avoid significant discomfort or exhaustion or (ii) repeatedly mobilise 200 meters within a reasonable timescale because of significant discomfort or exhaustion.', answerPoints: 6},
+      {id: 5, answerText: 'None of the above apply. 0 points.', answerPoints: 0}
+      ]},
 
-  @Output() checked = new EventEmitter();
-  @Input() question = {};
 
-  onCreateQuestion() {
-    const { title, value } = this.newQuestion;
-console.log(this.newQuestion)
-    if (title && value) {
-      this.createQuestion.next({ title, value });
-    }
+    {text: 'Don\'t forget to messsup', answers: [{id: 2, answerText: 'UJYTRTERE', answerPoints: 9}]},
 
-    this.reset();
+  ];
+
+
+  ngOnInit() {
+    console.log(this.newAssessment.question_1)
   }
 
-  reset() {
-    this.newQuestion = {
-      title: '',
-      value: ''
-    };
-  }
-  showCheck: boolean = true;
 
-  toggleCheck() {
-
-      this.showCheck = !this.showCheck
-
-  }
-
-  onChecked() {
-    this.checked.next(this.question);
-  }
+  // @Output() checked = new EventEmitter();
+  // @Input() question = {};
+  //
+  // onCreateQuestion() {
+  //   const { title, value } = this.newQuestion;
+  //   if (title && value) {
+  //     this.createQuestion.next({ title, value });
+  //   }
+  //
+  //   this.reset();
+  // }
+  //
+  //
+  // reset() {
+  //   this.newQuestion = {
+  //     title: '',
+  //     value: ''
+  //   };
+  // }
+  // showCheck: boolean = true;
+  //
+  // toggleCheck() {
+  //     this.showCheck = !this.showCheck
+  // }
+  //
+  // onChecked() {
+  //   this.checked.next(this.question);
+  // }
 
 }
