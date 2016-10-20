d8095b3d57e4efdc3aef983056b48d39395746e2
peel3r <ismc2222@gmail.com>
2016-10-20 15:12:05 +0100

    adds goal service to manager

---
+++ src/app/services/goal.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api';
+import 'rxjs/Rx';
+import { StoreHelper } from './store-helper';
+
+@Injectable()
+export class GoalService {
+  path: string = '/api/goals';
+  constructor(
+    private apiService: ApiService,
+    private storeHelper: StoreHelper
+  ) {}
+
+  createGoal(goal) {
+    return this.apiService.post(this.path, goal)
+      .do(savedGoal => this.storeHelper.add('diaries', savedGoal));
+  }
+
+  getGoals() {
+    return this.apiService.get(this.path)
+      .do(res => this.storeHelper.update('diaries', res.data));
+  }
+
+  getUserDiaries(userId) {
+    return this.getDiaries()
+      .map(diaries => diaries.filter(d => d.author._id === userId));
+
+
+  }
+
+  getGoal(id) {
+    return this.getDiaries()
+      .map(diaries => diaries.find(d => d._id === id));
+
+  }
+
+  completeGoal(goal) {
+    return this.apiService.delete(`${this.path}/${goal.id}`)
+      .do(res => this.storeHelper.findAndDelete('goals', res.id));
+
+  }
+}
