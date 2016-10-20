8e03e7822661facdabbc221664fe700973f33c66
peel3r <ismc2222@gmail.com>
2016-10-20 20:14:41 +0100

    adds goals button to navbar

--- src/app/ui/navbar/navbar-routes.config.ts
+++ src/app/ui/navbar/navbar-routes.config.ts
@@ -4,6 +4,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '', title: 'Home', menuType: MenuType.BRAND },
   { path: 'diaries', title: 'Pain Log', menuType: MenuType.RIGHT },
   { path: 'esa', title: 'Esa', menuType: MenuType.LEFT },
-  { path: 'pip', title: 'Pip', menuType: MenuType.LEFT }
+  { path: 'pip', title: 'Pip', menuType: MenuType.LEFT },
+  { path: 'goals', title: 'Goals', menuType: MenuType.RIGHT}
 
 ];
