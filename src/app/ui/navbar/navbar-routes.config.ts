9f6b42178ddb30690bf231e5faf8592c21d9b1dd
peel3r <ismc2222@gmail.com>
2016-10-29 19:48:00 +0100

    adds home button to navbar

--- src/app/ui/navbar/navbar-routes.config.ts
+++ src/app/ui/navbar/navbar-routes.config.ts
@@ -2,6 +2,8 @@ import { MenuType, RouteInfo } from './navbar.metadata';
 
 export const ROUTES: RouteInfo[] = [
   { path: '', title: 'Home', menuType: MenuType.BRAND },
+  { path: '', title: 'Home', menuType: MenuType.LEFT},
+
   { path: 'diaries', title: 'Pain Log', menuType: MenuType.RIGHT },
   { path: 'esa', title: 'Esa', menuType: MenuType.LEFT },
   { path: 'pip', title: 'Pip', menuType: MenuType.LEFT },
