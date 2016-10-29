3ac394764f8621aa90e6e7e28dfcd3ccc4f1608b
peel3r <ismc2222@gmail.com>
2016-10-29 19:33:09 +0100

    adds goalchart component

---
+++ src/app/goal/goal-charts.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'goal-chart',
+  templateUrl: './goal-chart.template.html'
+})
+export class DoughnutChartDemoComponent {
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+}
