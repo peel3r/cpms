3529ac4e548baa08044c83ddc17490332fe24a5d
peel3r <ismc2222@gmail.com>
2016-10-31 20:38:51 +0000

    adds activity model to server api

--- server/api/activity/activityModel.js
+++ server/api/activity/activityModel.js
@@ -2,12 +2,51 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ActivitySchema = new Schema({
-    name: {
-        type: String,
-        required: false,
-    },
+  name: {
+    type: String,
+    required: false,
+  },
+  fatigue: {
+    type: Number,
+    required: false
+  },
+  pain: {
+    type: Number,
+    required: false
+  },
+  fog: {
+    type: Number,
+    required: false
+  },
+  rating: {
+    type: Number,
+    required: false
+  },
+  comments: {
+    type: String,
+    required: false
+  },
+  relatedGoal: {
+    type: String,
+    required: false
+  },
+  done: {
+    type: Boolean,
+    required: false
+  },
+  duration: {
+    type: Number,
+    required: false
+  },
+  color: {
+    type: Number,
+    required: false
+  },
 
-    date: { type: Date, default: Date.now }
+
+  author: {type: Schema.Types.ObjectId, ref: 'user'},
+
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('activity', ActivitySchema);
