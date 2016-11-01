d26a44f4274cd89bfff979904b0983d665c5f2e8
peel3r <ismc2222@gmail.com>
2016-11-01 21:11:42 +0000

    adds activity index

---
+++ src/app/activity/index.ts
@@ -0,0 +1,4 @@
+export {Activity} from './activity.component';
+export {ActivityCard} from './activity-card.component';
+export {CreateActivity} from './create-activity';
+export * from './activity.component';
