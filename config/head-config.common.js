6562bcb66da69316465150542d1e21b92847083d
peel3r <ismc2222@gmail.com>
2016-11-03 21:12:55 +0000

    adds primeng and font awesome

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -33,6 +33,8 @@ module.exports = {
 
     { href: "https://fonts.googleapis.com/icon?family=Material+Icons",rel: "stylesheet"},
     { href: "/assets/ng2-toastr.min.css", rel:"stylesheet"},
+    { href: "/assets/css/primeng.min.css", rel:"stylesheet", inject: true },
+    { href: "/assets/css/theme.css", rel:"stylesheet", inject: true },
 
     { href: "/assets/jquery.min.js", type:"text/javascript"},
     { href: "/assets/bootstrap.min.js", type:"text/javascript"},
