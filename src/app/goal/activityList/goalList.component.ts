639818f442aaa5e36604783153ed8e5b2186bc72
peel3r <ismc2222@gmail.com>
2016-11-09 21:21:11 +0000

    adds delete save goal

--- src/app/goal/activityList/goalList.component.ts
+++ src/app/goal/activityList/goalList.component.ts
@@ -2,7 +2,7 @@ import {
   Component,
   OnDestroy,
 OnInit,
-Input
+Input, Output
 } from '@angular/core';
 import { Router } from "@angular/router";
 import { XLarge } from './x-large';
@@ -10,6 +10,7 @@ import { Store } from "../store";
 import 'rxjs/Rx'
 import {GoalService} from "../../services/goal.service";
 import {Goal} from "../goal.component";
+import {EventEmitter} from "@angular/core";
 
 @Component({
   selector: 'goal-list',
@@ -19,27 +20,47 @@ import {Goal} from "../goal.component";
 export class DataListDemo {
   USER_ID = window.localStorage.getItem('cpms_user_id')
   @Input() goals = [];
-
   selectedGoal: Goal;
-
+  updatedGoals = []
   displayDialog: boolean;
 
   constructor(private goalService: GoalService) { }
 
-  selectGoal(goal: Goal) {
+  selectGoal(goal) {
 
     this.selectedGoal = goal;
-    this.displayDialog = true;
+    this.displayDialog = !this.displayDialog;
+  }
+
+
+
+  deleteGoal(goal,i) {
+    console.log(goal)
+    this.goals.splice(i, 1);
+
+    this.goalService.deleteGoal(goal)
+      .subscribe()
+    // this.goalService.getUserGoals(this.USER_ID)
+    //   .subscribe(res => {
+    //     this.goals =  res.splice(i, 1);
+    //   })
+    this.displayDialog = !this.displayDialog;
+
   }
 
   onDialogHide() {
     this.selectedGoal = null;
   }
+
+  saveGoal(goal,i) {
+    //update
+    if(goal) {
+      console.log(goal)
+      this.goalService.completeGoal(goal)
+        .subscribe();
+    }
+
+    this.displayDialog = !this.displayDialog;
+  }
 }
 
-// export interface Goal {
-//   title;
-//   color;
-//   type;
-//   comments;
-// }
