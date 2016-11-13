712f2a71328dd7b40e9f826af3c6aa3ea20e7738
peel3r <ismc2222@gmail.com>
2016-11-13 22:00:08 +0000

    adds goals and activities durration arrays for chart

--- src/app/goal/summary/charts/chart.component.ts
+++ src/app/goal/summary/charts/chart.component.ts
@@ -10,72 +10,119 @@ import {GoalService} from "../../../services/goal.service";
 export class SummaryChartComponent {
 
   goals = []
+  goalsS = []
+
   activities = []
   goalsLength = []
   activitiesLength = []
   dateCount = []
   user_id = window.localStorage.getItem('cpms_user_id')
 
+  dailyActivities = []
 
 
+  goalActivities = []
+  goalActivitiesDuration = []
+
+  x = []
+  y =[]
   constructor( private activityService: ActivityService,
                private goalService: GoalService) {
 
   }
 
   ngOnInit() {
+
+
+      // .subscribe(res => this.activities = res)
+
     this.goalService.getUserGoals(this.user_id)
-      .map((goals) => {
-
-        if (goals) {
-
-          this.goals.push(goals)
-          goals.forEach((goal) => {
-              this.dateCount.push(goal.date.substring(5,10));
-
-              this.goalsLength.push(goal.duration)
-          }
-          );
-        }
-        return this.goalsLength.duration;
+      .map(goals => {
+        this.goals = goals
       })
-
-      .subscribe(res => this.goals = res)
+      .subscribe()
 
     this.activityService.getUserActivities(this.user_id)
-      .map((activities) => {
+      .map(activities => {
+        this.activities = activities
 
-        if (activities) {
 
-          this.activities.push(activities)
-          activities.forEach((activity) => {
-              this.activitiesLength.push(activity.duration)
-            }
-          );
-        }
-        return this.goalsLength.duration;
+        this.goals.forEach((goal,index) => {
+          this.goalsS.push(+goal.duration)
+          this.y.push(+goal.duration)
+          this.goalActivities.push(this.activities.filter((activity,i) => activity.relatedGoal === this.goals[index].title))
+  console.log('goal activities',this.goalActivities)
+
+          //sums goal activities duration times
+
+        })
+
+        this.goalActivities.forEach((goalActivity, i) => {
+          this.goalActivitiesDuration = goalActivity.reduce((prev,cur) => {
+
+            this.x.push(+prev.duration + +cur.duration)
+            console.log('cur',this.y)
+
+            // console.log('....',+prev[i].duration + +cur[i].duration)
+
+          })
+        })
+        console.log('goals',this.goalsS)
+
+        // this.goals.forEach(goal => {
+        //   // console.log('goal',goal)
+        //   this.activities.forEach(activity => {
+        //     if(goal.title === activity.relatedGoal) {
+        //       this.goalActivities.push([activity,goal]
+        //       )
+        //
+        //
+        //       // console.log('goalactivities',this.goalActivities)
+        //
+        //     }
+        //
+        //     // for (var index = 0; index < this.goalActivities.length; index++){
+        //     //   this.goalActivities[index]
+        //     //   console.log(this.goalActivities[index][1])
+        //     //
+        //     // }
+        //
+        //   })
+        //
+        // })
+        console.log(';',this.goalsS)
       })
+      .subscribe()
 
-      .subscribe(res => this.goals = res)
     setTimeout(() => {
-      this.randomize()
+
+      // this.randomize()
     }, 1000);
   }
 
 
+
+  // findEventIndexByRelatedGoal(title: string) {
+  //   for (let i = 0; i < this.activities.length; i++) {
+  //     if (title == this.activities[i].relatedGoal) {
+  //       this.dailyActivities.push(this.activities[i].relatedGoal)
+  //       break;
+  //     }
+  //   }
+  // }
 
   public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true
 
   };
-  public barChartLabels:string[] = this.dateCount;
+  public barChartLabels:string[] = this.goalsS;
   public barChartType:string = 'line';
   public barChartLegend:boolean = true;
 
   public barChartData:any = [
-    {data: this.goalsLength, label: 'Goals'},
-    {data: this.activitiesLength, label: 'Activities'},
+    {data: [30,23], label: 'Goals'},
+    {data: [45,11], label: 'Activities'},
   ];
 
   // events
@@ -89,7 +136,7 @@ export class SummaryChartComponent {
 
   public randomize() {
     // Only Change 3 values
-    let data = this.goalsLength;
+    let data = this.x;
     let clone = JSON.parse(JSON.stringify(this.barChartData));
     clone[0].data = data;
     this.barChartData = clone;
