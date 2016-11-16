8ee63d69aeed3e313356eafc9ba9dd2a741838f2
peel3r <ismc2222@gmail.com>
2016-11-16 22:28:06 +0000

    adds show component and template

---
+++ src/app/article/showArticle..component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit,Input} from '@angular/core';
+import {ArticleService} from '../services/articles'
+import {Router} from "@angular/router";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'show-article',
+  styleUrls: ['../article/article.style.css'],
+  templateUrl: 'showArticle.template.html'
+})
+
+export class ShowArticle {
+  @Input() article = {}
+  id: string
+
+  articles = []
+  user_id = window.localStorage.getItem('cpms_user_id')
+
+
+
+
+
+  constructor( private articleService: ArticleService, private route: ActivatedRoute, private router: Router) {
+
+    route.params.subscribe(params => { this.id = params['id']; });
+  this.articleService.getArticles()
+    .subscribe(
+      res => {this.articles = res})
+
+    this.articleService.getArticle(this.id)
+      .subscribe(res => this.article = res);
+  }
+
+  toUserProfile(): void {
+    this.router.navigate(['','users', this.user_id]);
+  }
+
+  onBack(): void {
+    this.router.navigate(['/articles']);
+  }
+}
