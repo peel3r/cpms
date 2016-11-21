ce7b956468dd2c175eed6ec2ae073d359763fec5
peel3r <ismc2222@gmail.com>
2016-11-21 23:12:28 +0000

    changes api url

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -10,7 +10,7 @@ export class ApiService {
     'Content-Type': 'application/json',
     Accept: 'application/json'
   });
-  apiUrl: string =  'http://localhost:3030' //'https://cpms-api.herokuapp.com' //
+  apiUrl: string =  'https://cpms-api.herokuapp.com' //'http://localhost:3030'
 
   constructor(private http: Http) {}
 
