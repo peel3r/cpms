bf8199b4d3785d2e0aea79b71632b9b544f9c854
peel3r <ismc2222@gmail.com>
2016-11-21 23:13:40 +0000

    adds activity table to activity list template and component

--- src/app/activity/activityList.component.ts
+++ src/app/activity/activityList.component.ts
@@ -6,117 +6,104 @@ import {
 } from '@angular/core';
 import { Router } from "@angular/router";
 import { XLarge } from './x-large';
-import { Store } from "../store";
 import 'rxjs/Rx'
-import {GoalService} from "../services/goal.service";
-import {Goal} from "../goal.component";
 import {EventEmitter} from "@angular/core";
-import {GoalList} from "../store";
+import {ActivityService} from "../services/activity.service";
+import {Store, ActivityList} from "../store";
+// import {Activity} from "./activity.component";
 
 @Component({
   selector: 'activity-table',
   templateUrl: './activityList.template.html',
 })
 
-export class ActivityList {
+export class ActivityTable {
   USER_ID = window.localStorage.getItem('cpms_user_id')
-  @Input() goals = [];
-  selectedGoal: MyEvent;
-  updatedGoals = []
-  event: MyEvent;
-  goal = {}
+
   displayDialog: boolean;
-  goalTypes = ['physical activity', 'mental activity', 'social activity'];
-  duration = ['5','10','15','20','30','45','60','90','120']
-  howOften = ['every day', 'every two days','twice a week', 'once a week', 'every fortnight'  ]
-  days = ['Morning','Noon', 'Afternoon','Evening','Late Evening','Night']
-  ratings = ['1','2','3','4','5','6','7','8','9','10']
 
-  constructor(private goalService: GoalService) { }
+  activity: Activity = new PrimeActivity ();
 
-  selectGoal(goal) {
+  selectedActivity : Activity ;
 
-    this.selectedGoal = goal;
-    this.displayDialog = !this.displayDialog;
+  newActivity : boolean;
+
+  activities: Activity [];
+
+
+  constructor(private activityService: ActivityService) {
+    this.activityService.getUserActivities(this.USER_ID)
+      .subscribe(res => this.activities = res )
+    console.log(this.activity)
+  }
+  showDialogToAdd() {
+    this.newActivity = true;
+    this.activity = new PrimeActivity();
+    this.displayDialog = true;
   }
 
+  save() {
+    if(this.newActivity)
+      this.activities.push(this.activity);
+    else
+      this.activities[this.findSelectedActivityIndex()] = this.activity;
 
-
-  deleteGoal(goal,i) {
-    let index: number = this.findEventIndexById(goal._id);
-    this.goals.splice(index, 1);
-
-    this.goalService.deleteGoal(goal)
-      .subscribe()
-
-
+    this.activity = null;
     this.displayDialog = false;
   }
 
-  findEventIndexById(id: number) {
-    let index = -1;
-    for(let i = 0; i < this.goals.length; i++) {
-      if(id == this.goals[i]._id) {
-        index = i;
-        break;
-      }
+  delete() {
+    this.activities.splice(this.findSelectedActivityIndex(), 1);
+    this.activity = null;
+    this.displayDialog = false;
+  }
+
+  onRowSelect(event) {
+    this.newActivity = false;
+    this.activity = this.cloneActivity(event.data);
+    this.displayDialog = true;
+  }
+
+  cloneActivity(a: Activity): Activity {
+    let activity = new PrimeActivity();
+    for(let prop in a) {
+      activity[prop] = a[prop];
     }
-
-    return index;
+    return activity;
   }
 
-
-
-  saveEvent() {
-
-    //update
-    console.log('this.selectedGoal', this.selectedGoal)
-    this.goalService.completeGoal(this.selectedGoal)
-      .subscribe();
-
-
-    this.displayDialog = false;
-
+  findSelectedActivityIndex(): number {
+    return this.activities.indexOf(this.selectedActivity);
   }
-
-  closeDialog() {
-    this.displayDialog = false;
-
-
-  }
-
-  onDialogHide() {
-    this.selectedGoal = null;
-  }
-
-
-
-  // saveGoal(goal,i) {
-  //   console.log(goal)
-  //
-  //   //update
-  //   if(goal) {
-  //     console.log(goal)
-  //     this.goalService.completeGoal(goal)
-  //       .subscribe();
-  //   }
-  //
-  //   this.displayDialog = false;
-  // }
 }
 
-export class MyEvent {
-  title: string
-  type: string
-  color?: string
-  duration?: string
-  howOften?: string
-  when?: string
-  comments?: string
-  confidenceLevel?: string
-  done: boolean
-  _id?: number
-  createdAt?: string
-  updatedAt?: string
-  userId?: string
+class PrimeActivity implements Activity {
+
+  constructor(  public fatigue?,
+                public pain?,
+                public fog?,
+                public rating?,
+                public comments?,
+                public relatedGoal?,
+                public color?,
+                public duration?,
+                public _id?,
+                public createdAt?,
+                public updatedAt?,
+                public userId?,) {}
+}
+
+export interface Activity {
+  fatigue?,
+  pain?,
+  fog?,
+  rating?,
+  comments?,
+  relatedGoal?,
+  color?,
+  duration?,
+  _id?,
+  createdAt?,
+  updatedAt?,
+  userId?,
 }
